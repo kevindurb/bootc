5408eda6eb59eda1d8e0deacee3585cc45807291
Kevin Durbin <kevindurb@fastmail.com>
2024-11-26 09:56:36 -0700

    fix: grab fedora version for rpm fusion and disable gnome and kde builds for now

--- scripts/rpm-fusion.sh
+++ scripts/rpm-fusion.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 dnf install -y \
-  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-41.noarch.rpm \
-  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-41.noarch.rpm
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
