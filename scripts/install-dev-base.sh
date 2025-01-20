6b769496ab16ebde86cab2629bd09b8d132ff7ae
Kevin Durbin <kevindurb@fastmail.com>
2025-01-19 21:42:25 -0700

    feat: add surface kernel, remove a bunch of passages ill install with brew

--- scripts/install-dev-base.sh
+++ scripts/install-dev-base.sh
@@ -9,28 +9,15 @@ dnf5 copr enable --assumeyes emanuelec/k9s
 # TODO: remove `--skip-unavailable` when this is resolved:
 # https://github.com/eza-community/eza/issues/1268
 dnf5 install --assumeyes --skip-unavailable \
-  ansible \
   alacritty \
-  bat \
-  bc \
   curl \
   diff \
   diffutils \
   distrobox \
-  duf \
-  entr \
-  eza \
-  fastfetch \
   find \
   findmnt \
-  fzf \
-  gh \
   git \
-  git-delta \
-  git-extras \
   gnupg2 \
-  go-task \
-  golang \
   gpg \
   gstreamer1-plugin-libav \
   gstreamer1-plugin-openh264 \
@@ -39,38 +26,16 @@ dnf5 install --assumeyes --skip-unavailable \
   gstreamer1-plugins-good \
   gstreamer1-plugins-ugly-free \
   heif-pixbuf-loader \
-  htop \
-  httpie \
-  jq \
-  just \
-  k9s \
-  kubectl \
-  lazygit \
-  litecli \
-  lolcat \
-  mycli \
-  ncdu \
-  neovim \
-  nyancat \
-  pgcli \
-  prettyping \
   pv \
   python-pip \
   python3-pip \
-  ripgrep \
   rsync \
   snapd \
-  starship \
   syncthing \
   tailscale \
-  tig \
-  tldr \
-  tmux \
   toolbox \
   wget \
   wl-clipboard \
-  yq \
-  zoxide \
   zsh
 dnf5 clean all
 
