18f46ca92565fb6f31cace38781e13490ab81eac
Kevin Durbin <kevindurb@fastmail.com>
2025-11-07 07:49:22 -0700

    add av1 and ffmpeg

--- scripts/install-dev-base.sh
+++ scripts/install-dev-base.sh
@@ -13,11 +13,13 @@ dnf5 install --assumeyes \
   diff \
   diffutils \
   distrobox \
+  ffmpeg \
   find \
   findmnt \
   git \
   gnupg2 \
   gpg \
+  gstreamer1-plugin-dav1d \
   gstreamer1-plugin-libav \
   gstreamer1-plugin-openh264 \
   gstreamer1-plugins-bad-free \
