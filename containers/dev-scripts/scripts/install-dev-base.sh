317642588c7b0682dabedc3476b9d156a79d11b6
Kevin Durbin <kevindurb@fastmail.com>
2026-07-27 10:24:59 -0600

    dont allowerasing

--- containers/dev-scripts/scripts/install-dev-base.sh
+++ containers/dev-scripts/scripts/install-dev-base.sh
@@ -7,7 +7,7 @@ dnf5 install --assumeyes \
 
 dnf5 install --assumeyes dnf5-plugins
 
-dnf5 install --assumeyes --allowerasing \
+dnf5 install --assumeyes \
   --exclude plasma-discover-snap \
   @development-tools \
   curl \
