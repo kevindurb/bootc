d7c99f311a73902d3b0399095b9d9f97abc282e5
Kevin Durbin <kevindurb@fastmail.com>
2026-07-27 10:55:24 -0600

    add bitwarden polkit

--- containers/dev-scripts/scripts/install-dev-base.sh
+++ containers/dev-scripts/scripts/install-dev-base.sh
@@ -47,3 +47,10 @@ systemctl --global enable syncthing.service
 systemctl enable tailscaled.service
 systemctl enable snapd.service
 /usr/bin/ln -sf /var/lib/snapd/snap /
+
+# Setup Bitwarden Biometrics
+sudo wget -O \
+  /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy \
+  https://raw.githubusercontent.com/bitwarden/clients/main/apps/desktop/resources/com.bitwarden.desktop.policy
+sudo chown root:root /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+sudo chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
