6fe42d065f15c67a966b1c1ed4ade0bc14b3576d
Kevin Durbin <kevindurb@fastmail.com>
2026-07-27 11:02:18 -0600

    fix

--- containers/dev-scripts/scripts/install-dev-base.sh
+++ containers/dev-scripts/scripts/install-dev-base.sh
@@ -52,5 +52,3 @@ systemctl enable snapd.service
 sudo wget -O \
   /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy \
   https://raw.githubusercontent.com/bitwarden/clients/main/apps/desktop/resources/com.bitwarden.desktop.policy
-sudo chown root:root /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
-sudo chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
