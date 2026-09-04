c210620d91162f9dc0c3e5e6dafbe619dbd9797b
Kevin Durbin <kevindurb@fastmail.com>
2026-09-04 12:44:02 -0600

    stable tailscale and davfs2

--- containers/dev-scripts/scripts/install-dev-base.sh
+++ containers/dev-scripts/scripts/install-dev-base.sh
@@ -5,6 +5,8 @@ dnf5 install --assumeyes \
     "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
     "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
+dnf5 config-manager --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+
 dnf5 install --assumeyes dnf5-plugins
 
 dnf5 install --assumeyes \
